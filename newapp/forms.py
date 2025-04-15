bc14a0eb73ad62bab91de328879f70d8b3a6b23f
Daniil <daniilgorbaachev2010@gmail.com>
2025-04-15 09:37:47 +0300

    forms asjdh

--- newapp/forms.py
+++ newapp/forms.py
@@ -2,9 +2,44 @@ from django import forms
 from .models import Company
 
 class CompanyForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Добавляем классы и атрибуты для всех полей
+        self.fields['name'].widget.attrs.update({
+            'class': 'form-control mb-3',
+            'placeholder': 'Введите название компании'
+        })
+        self.fields['region'].widget.attrs.update({
+            'class': 'form-control mb-3',
+            'placeholder': 'Введите регион'
+        })
+        self.fields['priority'].widget.attrs.update({
+            'class': 'form-control mb-3',
+            'min': '1',
+            'max': '10'
+        })
+        self.fields['contacts'].widget.attrs.update({
+            'class': 'form-control mb-3',
+            'rows': '3',
+            'placeholder': '{"phone": "+7 999 123-45-67", "email": "example@mail.com"}'
+        })
+        self.fields['activity_themes'].widget.attrs.update({
+            'class': 'form-control mb-3',
+            'placeholder': 'IT, Маркетинг, Производство'
+        })
+
     class Meta:
         model = Company
         fields = ['name', 'region', 'priority', 'contacts', 'activity_themes']
+        labels = {
+            'name': 'Название компании',
+            'region': 'Регион',
+            'priority': 'Приоритет (от 1 до 10)',
+            'contacts': 'Контактная информация (JSON)',
+            'activity_themes': 'Виды деятельности'
+        }
+        help_texts = {
+            'contacts': 'Введите данные в JSON-формате',
+            'activity_themes': 'Укажите через запятую'
+        }
         
-    contacts = forms.CharField(widget=forms.Textarea, help_text='Введите JSON: {"phone": "", "email": ""}')
-    activity_themes = forms.CharField(help_text='Введите через запятую: IT, Маркетинг')
